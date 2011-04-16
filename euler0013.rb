cb6bce30eee29d5c8929a59c30383935b2f3c0c2
Phil Rigovanov <phil@rigovanov.ru>
2011-04-16 16:42:14 +0400

    Решение eu 13

--- euler0013.rb
+++ euler0013.rb
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 # Найдите первые десять цифр суммы следующих ста 50-значных чисел.
 # Числа хранятся в файле euler0013.txt
+sum = 0
+IO.foreach ("euler0013.txt") do |line|
+  sum += line.to_i
+end
+puts sum.to_s[0,10]
