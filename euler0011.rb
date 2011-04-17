b48f939fa9b39a0d1259982edf860de318ff3078
Phil Rigovanov <phil@rigovanov.ru>
2011-04-17 16:51:24 +0400

    Решение eu 11

--- euler0011.rb
+++ euler0011.rb
@@ -5,44 +5,41 @@
 matrix = Array.new # Исходная таблица
 
 # Инициализация таблицы
-rows = 0
-columns = 0
 IO.foreach ("euler0011.txt") do |line|
-  rows += 1
-  row = line.split().map{ |elem| elem.to_i } 
-  columns = row.length
-  matrix << row
+  matrix << line.split().map{ |elem| elem.to_i } 
 end
+width = matrix[0].size
+height = matrix.size
 
 LEN = 4 # Длина цепочки
 products = Array.new # Массив произведений звений цепочек
 
-for i in 0..(rows-1)
-  for j in 0..(columns-LEN)
+for i in 0..(width-1)
+  for j in 0..(height-LEN)
     product = matrix[i][j]
     1.upto(LEN-1) {|k| product *= matrix[i][j+k]}
     products << product
   end
 end
-for i in 0..(rows-LEN)
-  for j in 0..(columns-1)
+for i in 0..(width-LEN)
+  for j in 0..(height-1)
     product = matrix[i][j]
     1.upto(LEN-1) {|k| product *= matrix[i+k][j]}
     products << product
   end
 end
-for i in 0..(rows-LEN)
-  for j in 0..(columns-LEN)
+for i in 0..(width-LEN)
+  for j in 0..(height-LEN)
     product = matrix[i][j]
     1.upto(LEN-1) {|k| product *= matrix[i+k][j+k]}
     products << product
   end
 end
-for i in (LEN-1)..(rows-1)
-  for j in 0..(columns-LEN)
+for i in (LEN-1)..(width-1)
+  for j in 0..(height-LEN)
     product = matrix[i][j]
     1.upto(LEN-1) {|k| product *= matrix[i-k][j+k]}
     products << product
   end
 end
-puts products.max
+puts "Максимальное произведение: #{products.max}"
